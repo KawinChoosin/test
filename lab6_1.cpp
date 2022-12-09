9b39da45a64fdaa5806a27ee6427bb9c62d9ee31
= <=>
2022-12-09 16:05:27 +0700

    edit lab6.1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int x=0;
+    int y=0;
+    int number=1;
+    while(number != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >>number;
+    if(number%2==0){
+        x++;
+    }else{
+        y++;
+    }
+}
+    cout << "#Even numbers = "<<x-1;
+    cout << "\n#Odd numbers = "<<y;
     return 0;
 }
